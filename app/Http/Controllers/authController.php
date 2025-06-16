a76c3f432a0492b5e9f87cadd573ae20bb0552aa
Emmanuel Mamani <emmanuelmamani100@gmail.com>
2025-06-16 16:47:47 -0400

    taller en proceso

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\student_workshop;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -80,5 +81,43 @@ class authController extends Controller
         return response()->json(['message' => 'Sesión cerrada exitosamente']);
     }
 
+    public function register_postulant(Request $request){
+        $student = student_workshop::where('ci',$request->ci)
+                    ->where('code_est',$request->code_est)
+                    ->where('birthday',$request->birtday)
+                    ->first();
+        if($student){
+            $user = User::create([
+                'name' => $student->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'ci'=>$student->ci,
+                'code_est'=>$student->code_est,
+                'birthday'=>$student->birthday,
+                'cellphone'=>$request->cellphone,
+                'telephone'=>$request->telephone,
+            ]);
 
+            $user->assignRole('student');
+            Auth::login($user);
+            $request->session()->regenerate();
+
+            return response()->json([
+                'message' => 'Usuario registrado exitosamente',
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'ci' => $user->ci,
+                    'code_est' => $user->code_est,
+                    'birthday' => $user->birthday,
+                    'cellphone' => $user->cellphone,
+                    'telephone' => $user->telephone,
+                    'role' => $user->getRoleNames()->first()
+                ]
+            ]);
+        }else{
+            return response()->json(['Postulante no encontrado'], 404);
+        }
+    }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cellphone')->unique();
+            $table->string('ci');
+            $table->string('telephone')->nullable();
+            $table->string('photo')->nullable();
+            $table->date('birthdate');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
